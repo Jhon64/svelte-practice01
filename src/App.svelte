7bedeade8f42f7685c9289807a353e1b5fb4be33
Jhon64 <jhtamaranto@gmail.com>
2020-06-18 22:23:09 -0500

    add sveltestrap

--- src/App.svelte
+++ src/App.svelte
@@ -14,14 +14,46 @@
     Label,
     Button
   } from "sveltestrap";
+  import ListProduct from "./components/ListProduct.svelte";
 
   let product = {
     id: "",
     name: "",
-    imageUrl: "",
+    imageUrl:"",
     description: "",
     category: ""
   };
+
+
+
+  $: listProduct = [
+    {id: 0, name: "laptop toshiba", description: "is a laptop", category: "laptops"},
+    {id: 1, name: "table ianix", description: "is a table", category: "tables"},
+  ]
+
+
+  const limpiar=e=>{
+    product = {
+      id: "",
+      name: "",
+      imageUrl:"",
+      description: "",
+      category: ""
+    };
+  }
+
+  const onSubmitHandle = e => {
+    let newProduct={
+      id:listProduct.length,
+      name:product.name,
+      description:product.description,
+      category:product.category,
+      imageUrl:product.imageUrl
+    }
+    listProduct=listProduct.concat(newProduct)
+    limpiar();
+    e.preventDefault()
+  }
 </script>
 
 <style>
@@ -29,15 +61,15 @@
 </style>
 
 <main>
-  <Container>
+  <Container class="mt-5">
     <Row>
-      <Col sm="6" mx="auto">
+      <Col sm="6" >
         <Card>
           <CardHeader>
             <CardTitle>Registro Productos</CardTitle>
           </CardHeader>
           <CardBody>
-            <Form>
+            <Form on:submit={onSubmitHandle}>
               <FormGroup>
                 <Label>Product name</Label>
                 <Input
@@ -54,21 +86,29 @@
               </FormGroup>
               <FormGroup>
                 <Label>imageProduct</Label>
-                <Input type="url" placeholder="productUrl" name="productUrl" />
+                <Input type="url"
+                       placeholder="productUrl"
+                       name="productUrl"
+                       bind:value={product.imageUrl}/>
               </FormGroup>
               <FormGroup>
                 <Label>Product category</Label>
-                <select class="form-control ">
-                  <option value="0">product 1</option>
-                  <option value="1">product 2</option>
+                <select class="form-control " bind:value={product.category}>
+                  <option value="laptops">Laptops</option>
+                  <option value="Pc's">Pc's</option>
+                  <option value="tables">Tables</option>
                 </select>
               </FormGroup>
               <FormGroup>
-                <Button class="btn-success" block size="md">save</Button>
+                <Button class="btn-success" block size="md" type="submit">save</Button>
               </FormGroup>
             </Form>
           </CardBody>
         </Card>
+      </Col>
+      <Col sm="6" >
+
+        <ListProduct listProduct={listProduct}></ListProduct>
       </Col>
     </Row>
   </Container>
